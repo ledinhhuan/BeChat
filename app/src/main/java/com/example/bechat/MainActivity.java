a7d8b3216813cffb6a284daa21b0e803b1a44154
Le Dinh Huan <huan.ld@neo-lab.vn>
2019-11-29 12:38:00 +0700

    fix design login

--- app/src/main/java/com/example/bechat/MainActivity.java
+++ app/src/main/java/com/example/bechat/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.bechat;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_main);
     }
 }

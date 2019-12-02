f6988ce94c3c4179559d9f906ce647af3cad77cb
Nguyen Thi Ngoc Len <len.ntn@workhouse.me>
2019-12-02 17:02:38 +0700

    layout login

---
+++ app/src/main/java/com/example/bechat/ui/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.bechat.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bechat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}

c17d0ca144ab6a333e720769eb1cd790d476e08b
Nguyen Thi Ngoc Len <len.ntn@workhouse.me>
2019-12-02 11:36:24 +0700

    layout splash

--- app/src/main/java/com/example/bechat/ui/MainActivity.java
+++ app/src/main/java/com/example/bechat/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.bechat;
+package com.example.bechat.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.bechat.R;
 
 public class MainActivity extends AppCompatActivity {
 
